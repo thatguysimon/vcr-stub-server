fa490c3b5c6e28ef8b44b30ddc710dab66e35054
Simon Nizov <simon.nizov@gmail.com>
2020-03-17 22:22:14 +0200

    Add fixture generator for PATCH request

--- tests/fixture_generator.py
+++ tests/fixture_generator.py
@@ -9,15 +9,16 @@ for file_name in os.listdir(FIXTURES_FOLDER):
 	print(f"Deleting {FIXTURES_FOLDER}/{file_name}")
 	os.remove(f"{FIXTURES_FOLDER}/{file_name}")
 
-# GET post
+
+print("Generating cassette for GET /posts/1")
 with vcr.use_cassette(f"{FIXTURES_FOLDER}/get_post.yaml"):
 	r = requests.get("https://jsonplaceholder.typicode.com/posts/1", headers={"Accept-Encoding": "gzip"})
 
-	# GET post
+print("Generating cassette for GET /posts")
 with vcr.use_cassette(f"{FIXTURES_FOLDER}/get_posts.yaml"):
 	r = requests.get("https://jsonplaceholder.typicode.com/posts", headers={"Accept-Encoding": "gzip"})
 
-# POST post
+print("Generating cassette for POST /posts")
 with vcr.use_cassette(f"{FIXTURES_FOLDER}/post_post.yaml"):
 	r = requests.post(
 		"https://jsonplaceholder.typicode.com/posts", 
@@ -25,5 +26,14 @@ with vcr.use_cassette(f"{FIXTURES_FOLDER}/post_post.yaml"):
 			"title": "foo",
 			"body": "bar",
       		"userId": 1
+  		}
+	)
+
+print("Generating cassette for PATCH /posts")
+with vcr.use_cassette(f"{FIXTURES_FOLDER}/patch_post.yaml"):
+	r = requests.patch(
+		"https://jsonplaceholder.typicode.com/posts/1", 
+		json={
+			"body": "baz"
   		}
 	)
