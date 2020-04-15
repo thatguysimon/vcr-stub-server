6d7411b68c3a3e5ff677ba4e27dbe56cb9e7bc82
Simon Nizov <simon.nizov@gmail.com>
2020-04-15 20:21:28 +0300

    Add readme and release information

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # vcr-stub-server
+
+This is a small tool for setting up a lightweight stub server that replays previously recorded VCR cassettes.
+
+Usually VCR is used internally while running the test suite. In that case, the library is responsible for intercepting HTTP requests. 
+
+But in some cases, it can be useful to be able to spin up a live HTTP server which given a preexisting VCR cassette, would respond to each request with its matching recorded response.
+
+One such case might come when implementing usage of [Pact](http://pact.io), where `vcr-stub-server` can be used to prevent the provider service from making requests to external services during pact verification. This project was inspired by Pact's own [Stub Service](https://github.com/pact-foundation/pact-mock_service#stub-service-usage).
+
+#### Caveats
+
+Unfortunately, there is no one standard for cassette YAML files, each VCR implementation is different. For example, cassette YAML files created by VCR.py won't be compatible with YAML files created by the Ruby implementation of VCR, etc.
+
+Therfore **this tool currently only supports [VCR.py](https://github.com/kevin1024/vcrpy)**, using the library's own implementation of parsing the YAML files.
+
+## Installation
+
+```
+pip install vcr-stub-server
+```
+
+## Usage
+
+Once the package is installed, use the `vcr-stub-server` command to spin up your stub server.
+
+```
+vcr-stub-server path/to/vcr_cassette.yml
+```
+
+## Contributing
+
+Bug reports and pull requests are welcome on GitHub at https://github.com/thatguysimon/vcr-stub-server. This project is intended to be a safe, welcoming space for collaboration, and contributors are expected to adhere to the [Contributor Covenant](https://contributor-covenant.org) code of conduct.
+
+## License
+
+The gem is available as open source under the terms of the [MIT License](https://opensource.org/licenses/MIT).
--- vcr_stub_server/__main__.py
+++ vcr_stub_server/__main__.py
@@ -12,62 +12,67 @@ from vcr_stub_server.cassettes.vcrpy_cassette import VcrpyCassette
 
 CASSETTE_FORMAT_TO_CLASS = {"vcrpy": VcrpyCassette}
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--bind",
-    "-b",
-    default="localhost",
-    metavar="ADDRESS",
-    help="Specify alternate bind address " "[default: all interfaces]",
-)
-parser.add_argument(
-    "--port",
-    "-p",
-    action="store",
-    default=8181,
-    type=int,
-    nargs="?",
-    help="Specify alternate port [default: 8000]",
-)
-parser.add_argument(
-    "--cassette-format",
-    "-cf",
-    action="store",
-    choices=CASSETTE_FORMAT_TO_CLASS.keys(),
-    default="vcrpy",
-    type=str,
-    help="Specify VCR implementation [default: vcrpy]",
-)
-parser.add_argument(
-    "path",
-    action="store",
-    default=8181,
-    type=str,
-    metavar="FILE",
-    help="Specify the VCR Casette file path",
-)
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--bind",
+        "-b",
+        default="localhost",
+        metavar="ADDRESS",
+        help="Specify alternate bind address " "[default: all interfaces]",
+    )
+    parser.add_argument(
+        "--port",
+        "-p",
+        action="store",
+        default=8181,
+        type=int,
+        nargs="?",
+        help="Specify alternate port [default: 8000]",
+    )
+    parser.add_argument(
+        "--cassette-format",
+        "-cf",
+        action="store",
+        choices=CASSETTE_FORMAT_TO_CLASS.keys(),
+        default="vcrpy",
+        type=str,
+        help="Specify VCR implementation [default: vcrpy]",
+    )
+    parser.add_argument(
+        "path",
+        action="store",
+        default=8181,
+        type=str,
+        metavar="FILE",
+        help="Specify the VCR Casette file path",
+    )
+    args = parser.parse_args()
 
-host = args.bind
-port = args.port
-cassette_path = args.path
+    host = args.bind
+    port = args.port
+    cassette_path = args.path
 
-if not os.path.isfile(cassette_path):
-    raise ValueError("Invalid cassette file path")
+    if not os.path.isfile(cassette_path):
+        raise ValueError("Invalid cassette file path")
 
-vcr_cassette = CASSETTE_FORMAT_TO_CLASS[args.cassette_format](
-    cassette_path=cassette_path
-)
-handler_class = BuildHandlerClassWithCassette(vcr_cassette=vcr_cassette)
+    vcr_cassette = CASSETTE_FORMAT_TO_CLASS[args.cassette_format](
+        cassette_path=cassette_path
+    )
+    handler_class = BuildHandlerClassWithCassette(vcr_cassette=vcr_cassette)
 
-print(time.asctime(), f"Starting VCR stub server for cassette: {cassette_path}")
-http_server = HTTPServer((host, port), handler_class)
+    print(time.asctime(), f"Starting VCR stub server for cassette: {cassette_path}")
+    http_server = HTTPServer((host, port), handler_class)
 
-try:
-    print(time.asctime(), f"Server started, serving on {host}:{port}")
-    http_server.serve_forever()
-except KeyboardInterrupt:
-    pass
+    try:
+        print(time.asctime(), f"Server started, serving on {host}:{port}")
+        http_server.serve_forever()
+    except KeyboardInterrupt:
+        pass
 
-http_server.server_close()
-print(time.asctime(), "Stopped server - %s:%s" % (host, port))
+    http_server.server_close()
+    print(time.asctime(), "Stopped server - %s:%s" % (host, port))
+
+
+if __name__ == "__main__":
+    main()
