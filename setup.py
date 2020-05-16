687787f2032ea60e503b3676a9c8c03799daae0b
Simon Nizov <simon.nizov@gmail.com>
2020-05-16 16:29:28 +0300

    Run black

--- setup.py
+++ setup.py
@@ -9,28 +9,31 @@ try:
 except ImportError:
     from distutils.core import setup
 
-readme = ''
+readme = ""
 
 setup(
     long_description=readme,
-    name='vcr-stub-server',
-    version='0.1.0',
-    description='Standalone stub server for replaying VCR cassettes',
-    python_requires='==3.*,>=3.6.0',
-    author='Simon Nizov',
-    author_email='simon.nizov@gmail.com',
-    packages=['vcr_stub_server', 'vcr_stub_server.cassettes'],
+    name="vcr-stub-server",
+    version="0.1.0",
+    description="Standalone stub server for replaying VCR cassettes",
+    python_requires="==3.*,>=3.6.0",
+    author="Simon Nizov",
+    author_email="simon.nizov@gmail.com",
+    packages=["vcr_stub_server", "vcr_stub_server.cassettes"],
     package_dir={"": "."},
     package_data={},
-    install_requires=['vcrpy==4.*,>=4.0.2'],
+    install_requires=["vcrpy==4.*,>=4.0.2"],
     extras_require={
         "dev": [
-            "black==19.*,>=19.10.0", "ipdb==0.*,>=0.13.2", "pytest==pytest-cov",
-            "pytest-cov==2.*,>=2.8.1", "requests==2.*,>=2.23.0"
+            "black==19.*,>=19.10.0",
+            "ipdb==0.*,>=0.13.2",
+            "pytest==pytest-cov",
+            "pytest-cov==2.*,>=2.8.1",
+            "requests==2.*,>=2.23.0",
         ]
     },
     scripts=["vcr_stub_server/__main__.py"],
-    entry_points = {
-        'console_scripts': ['vcr-stub-server=vcr_stub_server.__main__:main'],
-    }
+    entry_points={
+        "console_scripts": ["vcr-stub-server=vcr_stub_server.__main__:main"],
+    },
 )
